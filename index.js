26a5e50ddefcde796ae5aa433747127cdc02aa94
Matih Ur Rehman <matihurrehman.official@gmail.com>
2022-10-18 14:15:51 +0500

    post

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-const express = require("express")
+const express = require("express");
 const app = express();
 const port = 2000;
 app.listen(port)
+app.use(express.json())
 app.get("/",(req,resp)=>{
     resp.send(`
-    <script>
-    fucntion a(){
-        alert("java script is working")
-    }
-    <button onclick= a()>
-    hello 
-    </button>
-    </script>
-    `)
+    hello g its working now
+    <script>alert("java script is working")</script>`)
+    console.log(req.body)
+})
+app.post("/post",(req,resp)=>{
+    let opt=req.body
+resp.send("you can post");
+console.log(opt);
 })
