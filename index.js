778e7ffe0edad4f1dcc7aee056b9a19efe59bf5b
Matih Ur Rehman <matihurrehman.official@gmail.com>
2022-10-18 13:50:50 +0500

    testing packages inprojects

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log("test program ")
+const express = require("express")
+const app = express();
+const port = 2000;
+app.listen(port)
+app.get("/",(req,resp)=>{
+    resp.send(`
+    <script>
+    fucntion a(){
+        alert("java script is working")
+    }
+    <button onclick= a()>
+    hello 
+    </button>
+    </script>
+    `)
+})
